6378728395dcf517f17c419a5797aa349db086f5
AndreasFolomkin <66010938+AndreasFolomkin@users.noreply.github.com>
2020-05-27 14:08:23 +0300

    new commit2

--- src/main/java/com/proj/documents/CarDocument.java
+++ src/main/java/com/proj/documents/CarDocument.java
@@ -1,10 +1,7 @@
 package com.proj.documents;
 
 import com.proj.dto.car.CarDto;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @Getter @Setter
 @Document("cars")
+@Data
 
 public class CarDocument {
 @Id
